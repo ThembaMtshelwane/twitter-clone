e0244ba40f8e1dcfbc8c12857587d2d776e72491
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-08 09:17:18 +0200

    Join Options created

--- views/src/components/Auth/JoinOptions.tsx
+++ views/src/components/Auth/JoinOptions.tsx
@@ -1,5 +1,33 @@
+import { FcGoogle } from "react-icons/fc";
+
 const JoinOptions = () => {
-  return <div>JoinOptions</div>;
+  return (
+    <div className="my-10  row-span-2 max-w-[320px] mx-auto">
+      <h3 className="font-semibold text-3xl my-5 mx-auto sm:text-4xl">
+        Join today.
+      </h3>
+      <div className="flex flex-col">
+        <button className="flex items-center  bg-white text-primary rounded-2xl px-4 py-2">
+          <div className="flex items-center justify-center mx-auto space-x-4">
+            <FcGoogle />
+            <p className="">Sign in with Google</p>
+          </div>
+        </button>
+
+        <span className="grid grid-cols-[45%_10%_45%] items-center justify-center my-2">
+          <span className="border"></span>
+          <p className="text-center">or</p>
+          <span className="border"></span>
+        </span>
+
+        <button className="bg-secondary">Create account</button>
+        <p className="text-[12px] my-1">
+          By signing up, you agree to the Terms of Service and Privacy Policy,
+          including Cookie Use.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default JoinOptions;
--- views/src/pages/LandingPage.tsx
+++ views/src/pages/LandingPage.tsx
@@ -1,39 +1,19 @@
 import { FaXTwitter } from "react-icons/fa6";
-import { FcGoogle } from "react-icons/fc";
+import JoinOptions from "../components/Auth/JoinOptions";
 
 const LandingPage = () => {
   return (
-    <section className="grid grid-rows-8 items-center justify-center h-screen p-4">
-      <div className="text-5xl">
+    <section className="flex flex-col my-10 mx-auto h-screen p-4 max-w-[320px]">
+      <div className="text-5xl mb-10">
         <FaXTwitter />
       </div>
 
-      <h1 className="text-5xl font-semibold row-span-2">Happening now</h1>
+      <h1 className="text-5xl font-semibold row-span-2 sm:text-6xl">
+        Happening now
+      </h1>
+      <JoinOptions />
 
-      <div className="my-10  row-span-2">
-        <h3 className="font-semibold text-3xl my-4">Join today.</h3>
-        <div className="flex flex-col text-center">
-          <button className="flex items-center   bg-white text-primary rounded-2xl px-4 py-2">
-            <div className="flex items-center justify-center mx-auto space-x-4">
-              <FcGoogle />
-              <p className="">Sign in with Google</p>
-            </div>
-          </button>
-
-          <span className="grid grid-cols-[45%_10%_45%] items-center justify-center my-1">
-            <span className="border"></span>
-            <p className="text-center">or</p>
-            <span className="border"></span>
-          </span>
-
-          <button className="bg-secondary">Create account</button>
-          <p className="text-[12px] my-1">
-            By signing up, you agree to the Terms of Service and Privacy Policy,
-            including Cookie Use.
-          </p>
-        </div>
-      </div>
-      <div className="row-span-2">
+      <div className="row-span-2 max-w-[320px] mx-auto w-full">
         <p className="mb-2">Already have an account?</p>
         <button className="w-full text-secondary border">Sign in</button>
       </div>
