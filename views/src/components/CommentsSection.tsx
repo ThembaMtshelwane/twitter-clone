28239e25b10ac2ae3390504f8530d3da51aa77f1
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-12 09:17:03 +0200

    Minor changes in main layout

--- views/src/components/CommentsSection.tsx
+++ views/src/components/CommentsSection.tsx
@@ -4,16 +4,18 @@ const CommentsSection = () => {
   const data = [1, 2, 3, 4];
 
   return (
-    <div className=" h-[50vh] overflow-x-hidden overflow-y-auto relative  flex items-center justify-center">
-      <div className="absolute top-0 left-0 flex flex-col gap-5 justify-center w-full my-4">
-        <h2 className="text-3xl my-4 font-bold">{data?.length} Comments </h2>
-        {data !== null ? (
-          data.map((index) => <Tweet images={[]} id={index} key={index} />)
-        ) : (
-          <p>No comments yet</p>
-        )}
+    <>
+      <h2 className="text-3xl font-bold mt-8">{data?.length} Comments </h2>
+      <div className=" h-[50vh] overflow-x-hidden overflow-y-auto relative  flex items-center justify-center my-4">
+        <div className="absolute top-0 left-0 flex flex-col gap-5 justify-center w-full">
+          {data !== null ? (
+            data.map((index) => <Tweet images={[]} id={index} key={index} />)
+          ) : (
+            <p>No comments yet</p>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
