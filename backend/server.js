bd65a6c9af454c261beba42295fb10fe9dbd75a5
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-16 11:04:26 +0200

    deployment set up

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.route.js";
 import tweetRoutes from "./routes/tweet.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 9000;
+const __dirname = path.resolve()
 const corsOptions = {
   origin: "http://localhost:3000",
   methods: "GET,POST,PUT,DELETE,PATCH",
@@ -23,6 +25,13 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/api/users", userRoutes);
 app.use("/api/tweets", tweetRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,"/views/dist")))
+
+  app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "views", "dist", "index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   connectDB();
