f1ba1a5fb7441f96938c0f33708c549ed2a5f07d
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-16 13:32:28 +0200

    updates

---
+++ backend/api/server.js
@@ -0,0 +1,38 @@
+import serverless from "serverless-http";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+import tweetRoutes from "./routes/tweet.route.js";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 9000;
+const __dirname = path.resolve();
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE,PATCH",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use("/api/users", userRoutes);
+app.use("/api/tweets", tweetRoutes);
+app.use(router);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../views/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../views/dist", "index.html"));
+  });
+}
+
+module.exports.handler = serverless(app);
